46b5c7799f7408edd3efde94b36d8e86ffb9cb01
Nicolas Cahorel <nicolas.cahorel@gmail.com>
2024-05-07 20:30:13 +0200

    final commit and push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     annotationProcessor("com.google.dagger:hilt-compiler:${hiltVersion}")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.work:work-runtime:2.9.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
